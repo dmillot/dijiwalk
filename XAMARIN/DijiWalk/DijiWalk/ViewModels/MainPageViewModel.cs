f9a2d2ee42a8da9ad336971b3b86edd2f50ba0b8
Nathan GAGNIARRE <Nathan.GAGNIARRE@diiage.org>
2020-01-27 11:39:47 +0100

    Added base of the project

---
+++ XAMARIN/DijiWalk/DijiWalk/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DijiWalk.Models;
+using DijiWalk.Views;
+
+namespace DijiWalk.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+
+        public INavigationService NavigationService { get; private set; }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private MoyenTransport _moyenTransport;
+        public MoyenTransport MoyenTransport
+        {
+            get { return _moyenTransport; }
+            set { SetProperty(ref _moyenTransport, value); }
+        }
+
+        public DelegateCommand<MoyenTransport> GoToLogin { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+            MoyenTransport = new MoyenTransport { Id = 1, Libelle = "Piétons" };
+            this.GoToLogin = new DelegateCommand<MoyenTransport>(GoToLoginMethod);
+        }
+
+        void GoToLoginMethod(MoyenTransport moyenTransport)
+        {
+            var navParam = new NavigationParameters();
+            navParam.Add(nameof(DijiWalk.Models.MoyenTransport), moyenTransport);
+            this.NavigationService.NavigateAsync(nameof(LoginPage), navParam);
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            if (parameters.Any())
+            {
+                MoyenTransport = parameters[nameof(DijiWalk.Models.MoyenTransport)] as MoyenTransport;
+            }
+            
+        }
+
+
+    }
+}
