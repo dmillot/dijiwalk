c4650362b5318eef4f34be4ec0a7438096239276
Damien Millot <Damien.Millot@diiage.org>
2020-01-30 08:50:08 +0100

    Add routes for login, chat and jeu

--- DijiWalk.WebApplication/dijiwalk/src/router/index.js
+++ DijiWalk.WebApplication/dijiwalk/src/router/index.js
@@ -6,17 +6,29 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
     path: '/',
     name: 'home',
     component: Home
   },
   {
+    path: '/chat',
+    name: 'chat',
+    component: () => import('../views/Chat.vue')
+  },
+  {
+    path: '/jeu',
+    name: 'jeu',
+    component: () => import('../views/Jeu.vue')
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
