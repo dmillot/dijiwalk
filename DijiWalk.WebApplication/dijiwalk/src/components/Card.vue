0bfcaadffb4a49687b572a3c6dd5419b5da09ee4
Damien Millot <Damien.Millot@diiage.org>
2020-01-30 08:48:48 +0100

    Add card component

---
+++ DijiWalk.WebApplication/dijiwalk/src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-xs-12 col-md-4 col-grow">
+    <q-card @click="navigateToLink()" class="my-card full-height cursor-pointer">
+      <q-card-section class="flex column flex-center">
+        <q-icon :name="icon" class="q-mt-lg" style="font-size: 4em" />
+        <h5 class="text-bold q-ma-none q-mt-lg">{{ title }}</h5>
+        <p class="q-mt-xl text-center">{{ description }}</p>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: ['link', 'icon', 'title', 'description'],
+  methods: {
+    navigateToLink () {
+      this.$router.push(this.link)
+    }
+  }
+}
+</script>
