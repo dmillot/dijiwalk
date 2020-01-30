518c691d10830955b19b30a0401a51da53e145f7
Damien Millot <Damien.Millot@diiage.org>
2020-01-30 16:32:57 +0100

    Create Jeu view

--- DijiWalk.WebApplication/dijiwalk/src/views/Jeu.vue
+++ DijiWalk.WebApplication/dijiwalk/src/views/Jeu.vue
@@ -1,50 +1,190 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
-      <q-card @click="onClick" class="my-card full-height">
-        <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"
-              basic>
-          <div class="absolute-bottom text-subtitle2 text-center">
-            Title
+  <q-page class="q-px-xl">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title>DijiWalk</q-toolbar-title>
+
+        <div class="q-ml-md cursor-pointer non-selectable">
+            <q-icon name="fas fa-search" />
+            <q-menu>
+              <q-list bordered separator style="min-width: 100px">
+                <q-item>
+                  <q-select
+                    filled
+                    v-model="modelEquipe"
+                    use-input
+                    use-chips
+                    multiple
+                    input-debounce="0"
+                    label="Filtrer par équipe"
+                    :options="equipesOptions"
+                    @filter="filterEquipe"
+                    style="width: 250px"
+                  />
+                </q-item>
+                <q-item>
+                  <q-select
+                      filled
+                      v-model="modelParcours"
+                      use-input
+                      use-chips
+                      multiple
+                      input-debounce="0"
+                      label="Filtrer par parcours"
+                      :options="parcoursOptions"
+                      @filter="filterParcours"
+                      style="width: 250px"
+                    />
+                </q-item>
+                <q-item>
+                  <q-select
+                      filled
+                      v-model="modelParticipants"
+                      use-input
+                      use-chips
+                      multiple
+                      input-debounce="0"
+                      label="Filtrer par participants"
+                      :options="participantsOptions"
+                      @filter="filterParticipants"
+                      style="width: 250px"
+                    />
+                </q-item>
+              </q-list>
+            </q-menu>
           </div>
-        </q-img>
-      </q-card>
-  </div>
+      </q-toolbar>
+    </q-header>
+    <div class="row full-width justify-center q-pr-xl q-my-md q-col-gutter-xl">
+      <div class="col-xs-12 col-md-4 col-grow">
+        <q-card class="my-card full-height cursor-pointer flex column justify-center items-center bg-negative first-card">
+          <q-icon name="fas fa-plus text-white" style="font-size: 4em;" />
+        </q-card>
+      </div>
+      <div v-for="jeu in jeux" v-bind:key="jeu.id" class="col-xs-12 col-md-4 col-grow">
+        <q-card class="my-card">
+          <q-img src="https://images.frandroid.com/wp-content/uploads/2016/01/google-maps.png" />
+
+          <q-card-section>
+            <q-btn
+              fab
+              color="negative"
+              icon="fas fa-trash"
+              class="absolute"
+              style="top: 0; right: 12px; transform: translateY(-50%);"
+            />
+
+            <div class="row no-wrap">
+              <div class="col text-left text-bold text-h6 ellipsis">
+                {{ jeu.name }}
+              </div>
+            </div>
+            <div class="row items-center no-wrap text-grey">
+              <q-icon name="fas fa-calendar" />
+              <p class="q-ma-none q-ml-xs">{{ jeu.date }}</p>
+            </div>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions>
+            <q-btn flat round icon="fas fa-info-circle" />
+            <q-btn flat color="primary text-bold">
+              Informations
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
+const equipes = [
+  'Équipe 1', 'Équipe 2', 'Équipe 3', 'Équipe 4', 'Équipe 5'
+]
+
+const parcours = [
+  'Parcours 1', 'Parcours 2', 'Parcours 3', 'Parcours 4', 'Parcours 5'
+]
+
+const participants = [
+  'Damien', 'Nathan', 'Clément', 'Mathis', 'Eric'
+]
+
+const listeJeux = [
+  { id: 1, name: "Jeu 1", date: "30/02/2020" },
+  { id: 2, name: "Jeu 2", date: "30/03/2020" },
+  { id: 3, name: "Jeu 3", date: "30/04/2020" },
+  { id: 4, name: "Jeu 4", date: "30/05/2020" },
+  { id: 5, name: "Jeu 5", date: "30/06/2020" },
+  { id: 6, name: "Jeu 6", date: "30/07/2020" },
+  { id: 7, name: "Jeu 7", date: "30/08/2020" },
+]
+
 export default {
   name: 'jeu',
+  data () {
+    return {
+      modelEquipe: null,
+      modelParcours: null,
+      modelParticipants: null,
+      equipesOptions: equipes,
+      parcoursOptions: parcours,
+      participantsOptions: participants,
+      jeux: listeJeux
+    }
+  },
   methods: {
-    onClick () {
-      this.$router.push({ name: 'home' })
+    // navigateToJeu () {
+    //   this.$router.push({ name: 'home' })
+    // },
+    filterEquipe (val, update) {
+      update(() => {
+        if (val === '') {
+          this.equipesOptions = equipes
+        }
+        else {
+          const needle = val.toLowerCase()
+          this.equipesOptions = equipes.filter(
+            v => v.toLowerCase().indexOf(needle) > -1
+          )
+        }
+      })
+    },
+    filterParcours (val, update) {
+      update(() => {
+        if (val === '') {
+          this.parcoursOptions = parcours
+        }
+        else {
+          const needle = val.toLowerCase()
+          this.parcoursOptions = parcours.filter(
+            v => v.toLowerCase().indexOf(needle) > -1
+          )
+        }
+      })
+    },
+    filterParticipants (val, update) {
+      update(() => {
+        if (val === '') {
+          this.participantsOptions = participants
+        }
+        else {
+          const needle = val.toLowerCase()
+          this.participantsOptions = participants.filter(
+            v => v.toLowerCase().indexOf(needle) > -1
+          )
+        }
+      })
     }
   }
 }
 </script>
 
 <style scoped>
-.my-card {
-    width: 100%; max-width: 250px;
-    cursor: pointer;
-}
-
-.my-card:hover::before {
-    content: "";
-    position: absolute;
-    z-index: 10;
-    border-radius: 4px;
-    background-color: #000000;
-    opacity: 0.6;
-    width: 100%;
-    height: 100%;
-}
-
-.my-card:hover::after {
-    content: "";
-}
-
-.link-none {
-    color: #000000;
-    text-decoration: none;
+.first-card:hover {
+  background-color: #cc0016 !important;
 }
 </style>
